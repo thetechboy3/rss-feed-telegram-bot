6dd2bf8627175ec4eb716f8dfe5fc69318b8f3c4
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2021-10-12 16:08:42 +0530

    FIX: No entry will be skipped now
    Instead of checking just the first entry, now it will iterate over all
    the entries until the first one from the previous feed is reached

--- rss.py
+++ rss.py
@@ -2,8 +2,8 @@ import os
 import sys
 import feedparser
 from sql import db
-from time import sleep, time
-from pyrogram import Client, filters
+from time import sleep
+from pyrogram import Client
 from pyrogram.errors import FloodWait
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -14,10 +14,10 @@ try:
     feed_urls = list(set(i for i in os.environ["FEED_URLS"].split("|")))  # RSS Feed URL of the site.
     bot_token = os.environ["BOT_TOKEN"]   # Get it by creating a bot on https://t.me/botfather
     log_channel = int(os.environ["LOG_CHANNEL"])   # Telegram Channel ID where the bot is added and have write permission. You can use group ID too.
-    check_interval = int(os.environ.get("INTERVAL", 10))   # Check Interval in seconds.  
+    check_interval = int(os.environ.get("INTERVAL", 10))   # Check Interval in seconds.
     max_instances = int(os.environ.get("MAX_INSTANCES", 3))   # Max parallel instance to be used.
     str_session = os.environ.get("STR_SESSION")    #String session generate using your tg mobile number for sending mirror cmd on your behalf. Generate using python gen_str.py
-    mirr_chat = int(os.environ.get("MIRROR_CHAT_ID"))    #Group/chat_id of mirror chat or mirror bot to send mirror cmd
+    mirr_chat = int(os.environ.get("MIRROR_CHAT_ID", "-1"))    #Group/chat_id of mirror chat or mirror bot to send mirror cmd
     mirr_cmd = os.environ.get("MIRROR_CMD", "/mirror")    #if you have changed default cmd of mirror bot, replace this.
 except Exception as e:
     print(e)
@@ -38,11 +38,24 @@ if str_session is not None and str_session != "":
 def create_feed_checker(feed_url):
     def check_feed():
         FEED = feedparser.parse(feed_url)
+
         if len(FEED.entries) == 0:
             return
-        entry = FEED.entries[0]
-        if entry.id != db.get_link(feed_url).link:
-                       # ↓ Edit this message as your needs.
+
+        first_entry = FEED.entries[0]
+        last_id_from_db = db.get_link(feed_url).link
+
+        for entry_num, entry in enumerate(FEED.entries):
+
+            # Have reached the end of new entries
+            if entry.id == last_id_from_db:
+                db.update_link(feed_url, first_entry.id)
+                # No new entry
+                if entry_num == 0:
+                    print(f"Checked feed for {feed_url}: {entry.id}")
+                break
+
+            # ↓ Edit this message as your needs.
             message = f"**{entry.title}**\n```{entry.link}```"
             try:
                 app.send_message(log_channel, message)
@@ -55,8 +68,7 @@ def create_feed_checker(feed_url):
                 sleep(e.x)
             except Exception as e:
                 print(e)
-        else:
-            print(f"Checked RSS FEED: {entry.id}")
+
     return check_feed
 
 
