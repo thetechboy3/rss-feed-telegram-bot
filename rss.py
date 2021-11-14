4778531ea93d9111fef29073d0622591855f1d6e
Shubham Bagde <64890390+thetechboy3@users.noreply.github.com>
2021-11-14 08:50:40 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -62,7 +62,7 @@ def create_feed_checker(feed_url):
             try:
                 app.send_message(log_channel, message)
                 if app2 is not None:
-                    mirr_msg = f"{mirr_cmd} {first_entry.links[1].href}"
+                    mirr_msg = f"{mirr_cmd} {first_entry.link}"
                     app2.send_message(mirr_chat, mirr_msg)
             except FloodWait as e:
                 print(f"FloodWait: {e.x} seconds")
@@ -99,7 +99,7 @@ def create_feed_checker(feed_url):
             try:
                 app.send_message(log_channel, message)
                 if app2 is not None:   
-                    mirr_msg = f"{mirr_cmd} {entry.links[1]['href']}"
+                    mirr_msg = f"{mirr_cmd} {entry.link}"
                 elif "torlock.com" in entry.link:   
                     mirr_msg = f"{mirr_cmd} {entry.links[1]['href']}"
                     app2.send_message(mirr_chat, mirr_msg)
