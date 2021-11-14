62eeaacdff9ef6dfb997abe31056e1d11f69dbb1
Shubham Bagde <64890390+thetechboy3@users.noreply.github.com>
2021-11-14 09:45:52 +0530

    Update rss.py

--- rss.py
+++ rss.py
@@ -82,11 +82,11 @@ def create_feed_checker(feed_url):
                 break
 
             # ↓ Edit this message as your needs.
-            message = f"**{/mirror6}**\n```{entry.links[1]['href']}```"
+            message = f"**{mirr_cmd}**\n```{entry.links[1]['href']}```"
             try:
                 app.send_message(log_channel, message)
                 if app2 is not None:
-                    mirr_msg = f"{mirr_cmd} {entry.links[1]['href']}"
+                    mirr_msg = f"{entry.title} {entry.links[1]['href']}"
                     app2.send_message(mirr_chat, mirr_msg)
             except FloodWait as e:
                 print(f"FloodWait: {e.x} seconds")
